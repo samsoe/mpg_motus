f7f4ab344c6903a347a73a2a42eb2efc017431b9
samsoe <esamsoe@gmail.com>
2023-06-01 10:45:06 -0600

    Add mscofield dataset to R script

--- motus_eda.R
+++ motus_eda.R
@@ -3,14 +3,28 @@ library(tidyverse)
 
 project <- 'motus-mpg'
 
-bq_sql <-
+bq_sql_wb <-
   "
   SELECT *
   FROM 
     `motus-mpg.wblake_datadump.filtered`
   "
-df_bq <- bq_project_query(project, bq_sql)
-df_tb <- bq_table_download(df_bq)
-df <- as.data.frame(df_tb) 
 
-df %>% head()
+df_bq_wb <- bq_project_query(project, bq_sql_wb)
+df_tb_wb <- bq_table_download(df_bq_wb)
+df_wb <- as.data.frame(df_tb_wb) 
+
+df_wb %>% head()
+
+bq_sql_ms <-
+  "
+  SELECT *
+  FROM 
+    `motus-mpg.mscofield_motus.filtered`
+  "
+
+df_bq_ms <- bq_project_query(project, bq_sql_ms)
+df_tb_ms <- bq_table_download(df_bq_ms)
+df_ms <- as.data.frame(df_tb_ms) 
+
+df_ms %>% head()
